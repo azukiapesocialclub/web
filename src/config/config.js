ea245bd327ae24ce30db5c3e9db0136fbf352ce6
owenyuwono <michael.oswin@ovo.id>
2022-02-26 21:04:12 +0700

    fix: change contract address

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     1: {
-        contract_address: '0x532cC320C1CF91AF35d8837E07C17ad6553790Fb'
+        contract_address: '0x813b5c4ae6b188f4581aa1dfdb7f4aba44aa578b'
     },
     4: {
         contract_address: '0x5CafC2444A01Cbb02a4ff40929908960C92064eF'
